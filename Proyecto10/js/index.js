84524cc1b1f839d28565940a01c17fdcb278c3ab
Ruth15m <manriquezruth@hotmail.com>
2024-05-16 20:10:32 -0600

    "nuevApi"

--- Proyecto10/js/index.js
+++ Proyecto10/js/index.js
@@ -1,21 +1,23 @@
-let listaProductos = document.querySelector('#listaProductos');
+let listaProductos = document.querySelector("#listaProductos");
 
 
 function obtieneProductos(){
-    //Promise o promesa (funcion que no se sabe cuando se va a ejecutar)
-    fetch('https://fakestoreapi.com/products/')
-    .then(res=>res.json())
+    //Promise o promesa (funcion que no se sabe cuando se va a ejecutar)  
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
     .then(productosObtenidos => {
         console.log(productosObtenidos);
 
-        productosObtenidos.forEach(producto => {
-            console.log("producto: ", producto.title)
+        productosObtenidos.products.forEach((producto, indice) => {
+            console.log("producto: ", producto);
+
             listaProductos.innerHTML += `
             <div class="col-12 col-md-3">
                 <div class="card" >
-                    <img src="${producto.image}" class="imagenProducto card-img-top" alt="...">
+                    <img src="${producto.thumbnail}" class="img-imagenProducto card-img-top py-2 img-fluid" alt="..." style="height: 200px; object-fit: cover;">
                     <div class="card-body">
                         <h5 class="card-title">${producto.title}</h5>
+                        <p class="card-text">${producto.category}</p>
                         <p class="card-text">${producto.description.slice(0,100)}</p>
                         <p class="card-text text-danger" >$${producto.price}</p>
                         <a href="#" class="btn btn-primary">Comprar</a>
@@ -23,9 +25,8 @@ function obtieneProductos(){
                 </div>
             </div>
             `;
-        })
-    })
+        });
+    });
 }
 
 obtieneProductos();
-
